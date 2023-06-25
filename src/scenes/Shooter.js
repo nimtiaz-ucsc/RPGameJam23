af27e86e0858445713d3f8eb5a3aa68dcebf8cb4
nimtiaz-ucsc <nimtiaz@ucsc.edu>
2023-06-24 21:45:04 -0700

    created projectile group

--- src/scenes/Shooter.js
+++ src/scenes/Shooter.js
@@ -28,29 +28,46 @@ class Shooter extends Phaser.Scene {
 
         this.aim = this.physics.add.sprite(128, game.config.height-128, 'aim');
         this.player = this.physics.add.sprite(128, game.config.height-128, 'george');
-        this.projectileSpawn = this.physics.add.sprite(128, game.config.height-128, 'projectile').setScale(0.25).setOrigin(-2, 0.5);
+        this.projectileSpawn = this.physics.add.sprite(128, game.config.height-128, 'projectile').setScale(0.125).setOrigin(-4, 0.5).setVisible(false);
 
         this.projectiles = this.physics.add.group();
 
         this.projectileSpeed = 1000;
+        this.fireRate = 500;
         
     }
 
     update() {
         this.aimAngle = Phaser.Math.RAD_TO_DEG * Phaser.Math.Angle.Between(this.aim.x, this.aim.y, this.input.activePointer.x, this.input.activePointer.y);
+        if (this.aimAngle < -90) {
+            this.aimAngle = -90;
+        } else if (this.aimAngle > 0) {
+            this.aimAngle = 0;
+        }
         this.aim.setAngle(this.aimAngle);
         this.projectileSpawn.setAngle(this.aimAngle);
+
+        this.projectiles.children.entries.forEach(projectile => {
+            if (projectile.x > game.config.width + projectile.width) {
+                projectile.destroy();
+            }
+
+            if (projectile.y < 0 - projectile.width) {
+                projectile.destroy();
+            }
+        });
     }
 
     fireProjectile() {
         this.isFiring = true;
-        let projectile = this.physics.add.sprite(this.projectileSpawn.x, this.projectileSpawn.y, 'projectile').setAngle(this.aimAngle).setScale(0.25);
-        let velocity = this.physics.velocityFromAngle(this.aimAngle, this.projectileSpeed);
-        projectile.setVelocityX(velocity.x);
-        projectile.setVelocityY(velocity.y);
-        //this.projectiles.add(projectile);
+        let velocity = this.physics.velocityFromAngle(this.aimAngle, 1);
 
-        this.time.delayedCall(500, () => {
+        this.projectiles.create(this.projectileSpawn.x + velocity.x * 128, 
+                                this.projectileSpawn.y + velocity.y * 128, 'projectile')
+                                .setAngle(this.aimAngle).setScale(0.125)
+                                .setVelocity(velocity.x * this.projectileSpeed, velocity.y * this.projectileSpeed);
+
+        this.time.delayedCall(this.fireRate, () => {
             if (this.input.activePointer.leftButtonDown()) {
                 this.fireProjectile();
             } else {
